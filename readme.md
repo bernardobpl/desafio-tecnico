9ce7a13bda236c9cc183ef19e5710d4994a076b8
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2021-11-23 15:30:36 -0300

    init

---
+++ readme.md
@@ -0,0 +1,11 @@
+## To fetch dependencies:
+  - yarn
+
+## To start the server locally run:
+  - yarn start
+
+## API routes:
+| Verb | url | def | Payload expected|
+|--|--|--|--|
+  |GET |http://localhost:3000/clients| to list all clients| - |
+  |POST |http://localhost:3000/clients| to create a new client |{"name":  [[Customer name]]}
---
+++ server.js
@@ -0,0 +1,25 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const data = [];
+
+app.get("/clients", function(req, res){
+  res.json(data);
+});
+
+app.post("/clients", function(req, res){
+  if(req.body && req.body.name) {
+
+  const { name } = req.body;
+    data.push({id: data.length , name});
+    res.status(201).json(data);
+  } else {
+    res.sendStatus(400);
+  }
+
+});
+app.listen(3000, function() {
+  console.log("Server is running...");
+});
